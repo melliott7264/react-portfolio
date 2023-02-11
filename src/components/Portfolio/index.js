4594a9aa7130fa0d9cd73c5bdbe2e19b6e716ad6
Mark Elliott <markelliottva@gmail.com>
2023-02-11 14:53:01 -0500

    updated personal website info

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -28,7 +28,7 @@ const projectArray = [
     key: 2,
     title: "WordPress Demonstration",
     description:
-      "This single page WordPress website was the result of an exercise to convert the Photoshop mockup shown above into a fully responsive WordPress front page.  Click on the image to see the WordPress site.  It was created using a child theme of the Understrap parent theme with a fully custom front page using BootStrap for the navbar and including JavaScrip to create the What We Do Section.",
+      "This single page WordPress website was the result of an exercise to convert the Photoshop mockup shown above into a fully responsive WordPress front page.  Click on the image to see the WordPress site.  It was created using a child theme of the Understrap parent theme with a fully custom front page using BootStrap for the navbar and including JavaScript to create the What We Do Section.",
     deployedUrl: "https://demo.markelliottva.com/",
     imgUrl: "assets/images/pulse-tester.jpg",
     imgAlt: "Photoshop mockup of WordPress demonstration site",
@@ -37,7 +37,7 @@ const projectArray = [
     key: 3,
     title: "Mark Elliott's Personal Website",
     description:
-      "This website was reworked from Mark Elliott's last business website to share his traditional arts and crafts work.  This WordPress site was converted from Elementor to an child theme of the WordPress Twenty Twenty-Two theme.",
+      "This is Mark Elliott's personal website, transitioned from his last buisness site, to share his traditional arts and crafts work.  This website was recently redesigned using a custom child theme based on the Understrap parent theme.  It has a carousel on the landing page written from sctach by Mark Elliott using jQuery.",
     deployedUrl: "https://markelliottva.com/",
     imgUrl: "assets/images/MarkElliottVAScreenShot-1.jpg",
     imgAlt: "screenshot of MarkElliottVa.com",
